873cfdc6d2298e98c42c3cfb3aeb380e988c1e8d
jv110 <bebezos@hotmail.com>
2015-03-25 11:43:32 -0300

    Added Golf Club

--- Mod.js
+++ Mod.js
@@ -1,6 +1,10 @@
-//oh yeah
-ModPE.setItem(460, "axe", 1, "Machete");
+ModPE.setItem(460, "record_11", 0, "Machete");
+ModPE.setItem(461, "record_13", 0, "Golf Club");
 
 function attackHook(a, v) {
-if(getCarriedItem() == 460) Entity.setHealth(v, Entity.getHealth(v) - 10);
+if(getCarriedItem() == 460) {
+Entity.setHealth(v, Entity.getHealth(v) - 8);
+} else if(getCarriedItem() == 461) {
+Entity.setHealth(v, Entity.getHealth(v) - 5);
+}
 }
